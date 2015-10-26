ffff343c60e23f4f177a94c247a97ec7f25dce03
MARCOS ARMSTRONG <marcos.armstrong@mail.utoronto.ca>
2015-10-26 16:17:17 -0400

    Hi back

--- exercise1.py
+++ exercise1.py
@@ -11,16 +11,33 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
+# Describe your function
+
 
 def pig_latinify(word):
-    """
-    Describe your function
-Hi back
-    :param :
-    :return:
-    :raises:
 
-    """
-    result = ""
+    word = raw_input("Enter word:")
 
-    return result
+if len(word) > 0 and word.isalpha():
+    print word
+word = original.lower() or original.upper() or original.lower[1:]
+pig = "yay"
+first_letter = word[0]
+vowels = list("aeiou")
+
+pig_latin_word = word[1 : len(original)] + first_letter + pig
+if first_letter in word[0] == vowels:
+    pig_latin_word = word + pig[1:]
+
+else: print "try again"
+
+
+
+
+    # :param :
+    # :return:
+    # :raises:
+
+    # result = ""
+
+    # return result
