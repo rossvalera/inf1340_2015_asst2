f6d9962d1eb6338b3e41f724b145ef36176d504c
MARCOS ARMSTRONG <marcos.armstrong@mail.utoronto.ca>
2015-11-05 21:09:58 -0500

    edited comments, cleaned up syntax

--- exercise1.py
+++ exercise1.py
@@ -21,24 +21,24 @@ def pig_latinify(word):
     pig = "yay"
     # For this assignment we will consider y a vowel always
 
-    vowel = ["a","e","i","o","u"]
+    vowel = ["a", "e", "i", "o", "u"]
     index = 0
     found_letter = -1
 
-    # Make sure that all inputs are actual letters and not numbers
+    # Makes sure that all inputs are actual letters and not numbers
     if len(original) > 0 and original.isalpha():
-        # Check each letter of a given word to until first vowel is found
+        # Checks each letter of a given word to until first vowel is found
         for i in original:
             if i in vowel:
                 found_letter = index
                 break
-            index +=1
+            index += 1
 
         # no vowel found thus will return given word with "ay" at the end
         if found_letter == -1:
             no_vowel = original + pig[1:]
             return no_vowel
-        # First letter in the given word is a vowel will return the word with "yay" at the end
+        # When first letter in the given word is a vowel will return the word with "yay" at the end
         elif found_letter == 0:
             first_vowel = original + pig
             return first_vowel
